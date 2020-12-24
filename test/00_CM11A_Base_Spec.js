bfa1fd45add74a5ab20ece3a24c48b9833ba0527
Alan Thiessen <AlanDThiessen@gmail.com>
2020-12-24 15:49:39 -0500

    Enable unit testing with Jasmine.

---
+++ test/00_CM11A_Base_Spec.js
@@ -0,0 +1,41 @@
+/******************************************************************************
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Alan Thiessen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ ******************************************************************************/
+
+'use strict';
+
+const MockBinding = require('@serialport/binding-mock')
+const CM11A = require('../src/CM11A');
+
+
+describe('CM11A Base', function() {
+
+    it("creates a CM11A object", function(){
+        let cm11a = CM11A();
+
+        expect(typeof(cm11a)).toBe('object');
+    });
+
+});
